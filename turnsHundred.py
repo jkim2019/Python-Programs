56bb9fab746b9b70f82679ea691cd0d53e767464
John Kim <john.j.kim@vanderbilt.edu>
2016-10-19 14:49:18 -0500

    Adds repetition specification to turnsHundred

--- turnsHundred.py
+++ turnsHundred.py
@@ -3,14 +3,14 @@ import datetime
 def calcYear(curAge):
     '''
         pre: curAge is an int
-        post: return year in which person becomes 100
+        post: returns year in which person becomes 100
     '''
     now = datetime.datetime.now()
     return now.year + (100 - curAge)
 
 def queryAge():
     '''
-        query user for age and print when person becomes 100
+        queries user for age
     '''
 
     curAge = input('Enter your current age: ')
@@ -18,6 +18,20 @@ def queryAge():
         print 'please input a valid age'
         curAge =  input('Enter your current age: ')
 
-    print 'You will turn 100 in the year %d' % calcYear(curAge)
+    year = calcYear(curAge)
+    repeatAns(year)
+
+# additional specifications
+
+def repeatAns(year):
+    '''
+        pre: year is a number
+        queries user for a number and repeats message n times
+    '''
+
+    numRepeat = input('Enter the number of times you would like me to repeat myself: ')
+    while numRepeat > 0:
+        print "You will turn 100 years old in the year %d." % year
+        numRepeat -= 1
 
 queryAge()
