6ae589b624412f4a5b63a694af2b6eae24206632
John Kim <john.j.kim@vanderbilt.edu>
2016-10-19 14:58:26 -0500

    asks for name

--- turnsHundred.py
+++ turnsHundred.py
@@ -8,22 +8,22 @@ def calcYear(curAge):
     now = datetime.datetime.now()
     return now.year + (100 - curAge)
 
-def queryAge():
+def queryDetails():
     '''
-        queries user for age
+        queries user for name and age
     '''
-
+    myName = input('Enter your name: ')
     curAge = input('Enter your current age: ')
     while curAge <= 0 or type(curAge) != int:
         print 'please input a valid age'
         curAge =  input('Enter your current age: ')
 
     year = calcYear(curAge)
-    repeatAns(year)
+    repeatAns(myName, year)
 
 # additional specifications
 
-def repeatAns(year):
+def repeatAns(name, year):
     '''
         pre: year is a number
         queries user for a number and repeats message n times
@@ -31,7 +31,7 @@ def repeatAns(year):
 
     numRepeat = input('Enter the number of times you would like me to repeat myself: ')
     while numRepeat > 0:
-        print "You will turn 100 years old in the year %d." % year
+        print "%s will turn 100 years old in the year %d." % (name, year)
         numRepeat -= 1
 
-queryAge()
+queryDetails()
